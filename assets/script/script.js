86b85b9027e805f597e5f6ba0ccec352998768c4
JeremySynakowski <jeremy.synakowski@gmail.com>
2021-01-09 17:09:00 +0100

    ajout page d'accueil

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,20 @@
 'use strict'
+
+function scrollRight(){
+    let navbar = document.getElementById('navbar');
+    navbar.style.left= '0px';
+    navbar.style.transition = 'all 1s'
+}
+
+function scrollLeft(){
+    let navbar = document.getElementById('navbar');
+    navbar.style.left= '-1000px';
+    navbar.style.transition = 'all 2s'
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('menu').addEventListener("click", scrollRight);    
+    document.getElementById('closeCross').addEventListener("click", scrollLeft);    
+
+});
+
